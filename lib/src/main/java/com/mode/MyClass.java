423f7cf079792bcd26e2ff8555adbc21c129a4fe
xiangxiang.liu <liuxiangxiang1234@163.com>
2016-10-27 23:27:53 +0800

    first commit

---
+++ lib/src/main/java/com/mode/MyClass.java
@@ -0,0 +1,9 @@
+package com.mode;
+
+public class MyClass {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        String string = "abc";
+        System.out.println(string);
+    }
+}

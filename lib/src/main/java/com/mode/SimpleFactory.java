1e2ae5ccd96f536aa59180802827dc8828da82f6
xiangxiang.liu <xiangxiangliug@tcl.com>
2016-10-28 15:16:54 +0800

    新增工厂类

---
+++ lib/src/main/java/com/mode/SimpleFactory.java
@@ -0,0 +1,14 @@
+package com.mode;
+
+/**
+ * @author Jerry
+ * @Description:
+ * @date 2016/10/28 10:52
+ * @copyright TCL-MIG
+ */
+
+public class SimpleFactory {
+    public static void main(String[] args) {
+
+    }
+}

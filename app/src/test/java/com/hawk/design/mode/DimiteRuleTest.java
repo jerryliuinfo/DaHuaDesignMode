b0fbb59ef73ce4984a41ebd6c142d1ae223bd004
xiangxiang.liu <liuxiangxiang1234@163.com>
2016-11-07 23:31:24 +0800

    模板模式

---
+++ app/src/test/java/com/hawk/design/mode/DimiteRuleTest.java
@@ -0,0 +1,11 @@
+package com.hawk.design.mode;
+
+/**
+ * 迪米特法则: 如果两个类不必彼此直接通信，那么这两个类就不应该发生直接的相互作用，如果
+ * 其中一个类需要调用另一个类的方法的话，可以通过第三者转发这个调用
+ * Created by lenovo on 2016/11/6.
+ */
+
+public class DimiteRuleTest {
+
+}
---
+++ app/src/test/java/com/hawk/design/mode/dimterule/Girl.java
@@ -0,0 +1,8 @@
+package com.hawk.design.mode.dimterule;
+
+/**
+ * Created by lenovo on 2016/11/6.
+ */
+
+public class Girl {
+}
